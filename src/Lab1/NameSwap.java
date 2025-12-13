436c95f5edd9b95bc8c2c2230ed4a72a343fedc7
aasthasinha31 <aasthasinha201@gmail.com>
2025-12-13 23:56:39 +0530

    Lab 1

---
+++ src/Lab1/NameSwap.java
@@ -0,0 +1,15 @@
+package Lab1;
+import java.util.Scanner;
+
+public class NameSwap {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+
+        System.out.println(lastName + " " + firstName);
+    }
+}
+

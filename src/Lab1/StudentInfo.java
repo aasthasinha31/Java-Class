2c82a3c1e054f6209e11861e581765f4c03f2a96
aasthasinha31 <aasthasinha201@gmail.com>
2025-12-13 23:42:44 +0530

    info

---
+++ src/Lab1/StudentInfo.java
@@ -0,0 +1,11 @@
+package Lab1;
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Aastha Sinha");
+        System.out.println("Roll No: 24052599");
+        System.out.println("Section: 7");
+        System.out.println("Branch: CSE");
+    }
+}
+
